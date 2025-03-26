914a733b80857e6007f358280c7d3d69548b8b47
Carlosrdsr <carlosrdsr@gmail.com>
2025-03-26 16:29:11 -0300

    finalizado desafio 2.

--- Questao2/Program.cs
+++ Questao2/Program.cs
@@ -1,18 +1,20 @@
 ﻿using Newtonsoft.Json;
+using Questao2.Domain.Model;
 
 public class Program
 {
-    public static void Main()
+    private const string urlBase = @"https://jsonmock.hackerrank.com/api/football_matches";
+    public static async Task Main()
     {
         string teamName = "Paris Saint-Germain";
         int year = 2013;
-        int totalGoals = getTotalScoredGoals(teamName, year);
+        int totalGoals = await getTotalScoredGoals(teamName, year);
 
         Console.WriteLine("Team "+ teamName +" scored "+ totalGoals.ToString() + " goals in "+ year);
 
         teamName = "Chelsea";
         year = 2014;
-        totalGoals = getTotalScoredGoals(teamName, year);
+        totalGoals = await getTotalScoredGoals(teamName, year);
 
         Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
 
@@ -21,10 +23,62 @@ public class Program
         // Team Chelsea scored 92 goals in 2014
     }
 
-    public static int getTotalScoredGoals(string team, int year)
+    public static async Task<int> getTotalScoredGoals(string team, int year)
     {
-        
-        return 0;
+        int totalGoals = 0;
+
+        totalGoals += await GetGoalsFromApi(team, year, true);
+
+        totalGoals += await GetGoalsFromApi(team, year, false);
+
+        return totalGoals;
     }
 
+    public static async Task<int> GetGoalsFromApi(string team, int year, bool isTeam1)
+    {
+        int totalGoals = 0;
+        int page = 1;
+        int totalPages = 1;
+
+        do
+        {
+            string url = $"{urlBase}?year={year}&{(isTeam1 ? "team1" : "team2")}={team}&page={page}";
+
+            ApiResponseModel response = await GetCompetitionDataAsync(url);
+
+            if (response == null)
+                break;
+
+            foreach (var match in response.Data)
+            {
+                if (isTeam1)
+                {
+                    if (int.TryParse(match.Team1Goals, out int team1Goals))
+                        totalGoals += team1Goals;
+                }
+                else
+                {
+                    if (int.TryParse(match.Team2Goals, out int team2Goals))
+                        totalGoals += team2Goals;
+                }
+            }
+
+            totalPages = response.TotalPages;
+            page++;
+
+        } while (page <= totalPages);
+
+        return totalGoals;
+    }
+
+    private static async Task<ApiResponseModel> GetCompetitionDataAsync(string url)
+    {
+        using HttpClient client = new();
+
+        HttpResponseMessage response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode(); 
+
+        string jsonResponse = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<ApiResponseModel>(jsonResponse);
+    }
 }
