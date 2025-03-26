4daae8457c9bf8b3d9e7e5f4e9474f1fa9d9ddc2
Carlosrdsr <carlosrdsr@gmail.com>
2025-03-26 12:05:25 -0300

    finalizado desafio 1.

--- Questao1/obj/Debug/net6.0/Questao1.AssemblyInfo.cs
+++ Questao1/obj/Debug/net6.0/Questao1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Questao1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+320a185b2434c5bd78b6c22b8a8058778c0e170f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Questao1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Questao1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
